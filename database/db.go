80b05f176c1efdabe0597741463279dce1fd66e7
Nathan <nslee333@gmail.com>
2024-02-09 19:03:35 -0800

    updated connection dsn with env vars

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,7 +17,14 @@ type Url struct {
 }
 
 func db() {
-	dsn := ""
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Pacific",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
