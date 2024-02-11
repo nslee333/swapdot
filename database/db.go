8211e486fbc9a25f6077330b7fbbd4bfce76612c
Nathan <nslee333@gmail.com>
2024-02-10 16:42:35 -0800

    adhering to conventions

--- database/db.go
+++ database/db.go
@@ -16,7 +16,7 @@ type Url struct {
 	short_url string
 }
 
-func Connect_to_database() (**gorm.DB, error) {
+func ConnectToDatabase() (**gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Pacific",
 		os.Getenv("DB_HOST"),
@@ -25,24 +25,52 @@ func Connect_to_database() (**gorm.DB, error) {
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
+	// fmt.Println(os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_PORT"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
+	// fmt.Sprintf(dsn)
+
 	if err != nil {
-		fmt.Printf("Error connecting to Database: %s", err)
+		// fmt.Printf("Error connecting to Database: %s, dsn: %s", err, dsn)
+
+		fmt.Println(os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"))
+
 	}
 
 	return &db, err
 }
 
-func Insert_url() {
+func main() {
+	db, err := ConnectToDatabase()
+
+	if err != nil {
+		fmt.Println("")
+	}
+
+	// db
+}
+
+func GetUrl(db **gorm.DB) {
 
 }
 
-func Update_url() {
+func InsertUrl(db **gorm.DB) {
 
 }
 
-func Delete_url() {
+func UpdateUrl(db **gorm.DB) {
+
+}
+
+func DeleteUrl(db **gorm.DB) {
 
 }
